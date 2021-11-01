880e2ab6203dc96471680ee17d1dc23b2eb053d1
t.hassan <tajiri.hassan@gmail.com>
2021-11-01 19:24:42 +0100

    starting a php mail form

---
+++ sendemail.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$^message = $_POST['message'];
+
+$mailheader = "Form:".$name."<".email.">"\r\n":
+
+$recipient = "tajiri.hassan@gmail.com";
+
+mail($recipient, $subject, $emailheader)
+
+or die("Error!");
+
+echo"message sent";
+
+
+
+
+
+?>
